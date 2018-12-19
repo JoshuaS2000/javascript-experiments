527e145c6228f2b3052ab79c1cf2e48690ba1ef1
JoshuaS2000 <joshuamstokes2000@gmail.com>
2018-12-19 09:01:52 -0600

    Update main.js

--- testgame/main.js
+++ testgame/main.js
@@ -1,8 +1,23 @@
 //TestGame main
 //Author: Joshua S
 
-function main(){
-    
-}
+// The application will create a renderer using WebGL, if possible,
+// with a fallback to a canvas render. It will also setup the ticker
+// and the root stage PIXI.Container
+const app = new PIXI.Application();
 
-main();
+// The application will create a canvas element for you that you
+// can then insert into the DOM
+document.body.appendChild(app.view);
+
+// load the texture we need
+PIXI.loader.add('logo', '/assets/logo.png').load((loader, resources) => {
+    // This creates a texture from a 'bunny.png' image
+    const logo = new PIXI.Sprite(resources.logo.texture);
+
+    // Setup the position of the bunny
+    bunny.x = app.renderer.width / 2;
+    bunny.y = app.renderer.height / 2;
+    
+    
+});
